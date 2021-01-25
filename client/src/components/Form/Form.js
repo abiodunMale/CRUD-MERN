7b1e701a13dab0e3f5f689ec327c6471f0de847b
unknown <abiodun.samuel@feghas.com>
2021-01-25 17:09:27 +0100

    client side

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import useStyles from './styles';
+
+const From = () => {
+
+    const classes = useStyles(); 
+    
+    return(
+        <h1>FORM</h1>
+    );
+}
+
+export default From;
